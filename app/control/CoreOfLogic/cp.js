b48e9f6a0449b5f888f00fd1cd4b4d6e10eae7e3
ThomasFuzhou <29013551@qq.com>
2018-11-19 20:15:57 +0800

    简化服务器端的处理，将 控制器 cp.js 设置为接收 paramGold.items 数组参数模式，即原样接受客户端参数传递的数组，传递到链上处理。

--- app/control/CoreOfLogic/cp.js
+++ app/control/CoreOfLogic/cp.js
@@ -5,7 +5,8 @@ let {ReturnCode, NotifyType} = facade.const
 const toolkit = require('gamegoldtoolkit')
 //创建授权式连接器实例
 const remote = new toolkit.conn();
-remote.setFetch(require('node-fetch'))  //兼容性设置，提供模拟浏览器环境中的 fetch 函数
+//兼容性设置，提供模拟浏览器环境中的 fetch 函数
+remote.setFetch(require('node-fetch'))  
 
 /**
  * 游戏的控制器
@@ -21,71 +22,30 @@ class cp extends facade.Control
     }
 
     /**
-     * 增
+     * CP注册指令：cp.create "name" "url" ["ip"]
      * @param {*} user 
-     * @param {*} objData 
+     * @param {*} paramGold 其中的成员 items 是传递给区块链全节点的参数数组
      */
-    async Create(user, objData) {
-        /**
-        let test = await facade.GetMapping(101).Create(Math.random().toString());
-        return {code: ReturnCode.Success, data: test.item};
-         */
+    async Create(user, paramGold) {
+        console.log(paramGold.items[0]);
+        console.log(paramGold.items[1]);
+        console.log(paramGold.items[2]);
+        let ret = await remote.execute('cp.create', paramGold.items);//数组参数调用，无参数时传入空数组
+        console.log(ret);
+        return {code: ReturnCode.Success,result: ret};
     }
 
     /**
-     * 改
+     * 查询系统中现有的所有CP列表：cp.list
      * @param {*} user 
-     * @param {*} objData 
+     * @param {*} paramGold 其中的成员 items 是传递给区块链全节点的参数数组
      */
-    Update(user, objData) {
-        /**
-        let test = facade.GetObject(101, objData.id);           //根据上行id查找test表中记录
-        if(!!test) {
-            test.setAttr('item', Math.random().toString());     //修改所得记录的item字段，下次查询时将得到新值，同时会自动存入数据库
-            return {code: ReturnCode.Success, data: test.getAttr('item')};
-        }
-         */
-        return {code: -1};
-
-    }
-
-    /**
-     * 查
-     * @param {*} user 
-     * @param {*} objData 
-     */
-    Retrieve(user, objData) {
-        //根据上行id查找test表中记录, 注意在 get 方式时 id 不会自动由字符串转换为整型
-        let test = facade.GetObject(101, parseInt(objData.id));
-        console.log("控制器添加日志：");
-        console.log(test);
-        console.log(objData.id);
-        if(!!test) {
-            return {code: ReturnCode.Success, data: test.getAttr('item')};
-        }
-        return {code: -1};
-    }
-
-    /**
-     * 删
-     * @param {*} user 
-     * @param {*} objData 
-     */
-    Delete(user, objData) {
-        /**
-        facade.GetMapping(101).Delete(objData.id, true);
-        return {code: ReturnCode.Success};
-         */
-    }
-
-    /**
-     * cp.list 列表
-     * @param {*} user 
-     * @param {*} objData 
-     */
-    async List(user, objData) {
-        console.log("cp.js line 86");
-        let ret = await remote.execute('cp.list', []);
+    async List(user, paramGold) {
+        console.log(paramGold.items[0]);
+        console.log(paramGold.items[1]);
+        console.log(paramGold.items[2]);
+        console.log(paramGold.items[3]);
+        let ret = await remote.execute('cp.list', paramGold.items);//数组参数调用，无参数时传入空数组
         console.log(ret);
         return {code: ReturnCode.Success,list: ret};
     }
--- test/auth.js
+++ test/auth.js
@@ -40,16 +40,16 @@ describe('认证', function() {
         let msg = await remote.login({openid: `${Math.random()*1000000000 | 0}`});
         if(remote.isSuccess(msg)) {
             //所有的控制器都拥有echo方法
-            msg = await remote.fetching({func: "cp.List"});
+            msg = await remote.fetching({func: "cp.List",items:[]});
             remote.isSuccess(msg, true);
         }
     });
 
-    it.only('test.Retrieve', async () => {
+    it('cp.create', async () => {
         let msg = await remote.login({openid: `${Math.random()*1000000000 | 0}`});
         if(remote.isSuccess(msg)) {
             //所有的控制器都拥有echo方法
-            msg = await remote.fetching({func: "test.Retrieve",id:2});
+            msg = await remote.fetching({func: "cp.Create",items:["testfellowX","http://920.cc"]});
             remote.isSuccess(msg, true);
         }
     });
