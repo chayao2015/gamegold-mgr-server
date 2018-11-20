10b9f6a1e5b4584027890dc5575dcbd816e06c5d
ThomasFuzhou <29013551@qq.com>
2018-11-20 14:49:02 +0800

    fix

--- app/control/CoreOfLogic/cp.js
+++ app/control/CoreOfLogic/cp.js
@@ -22,34 +22,64 @@ class cp extends facade.Control
     }
 
     /**
+     * 查询系统中现有的所有CP列表：cp.list
+     * @param {*} user 
+     * @param {*} paramGold 其中的成员 items 是传递给区块链全节点的参数数组
+     */
+    async List(user, paramGold) {
+        console.log(paramGold.items);
+        let ret = await remote.execute('cp.list', paramGold.items);
+        console.log(ret);
+        return {code: ReturnCode.Success,list: ret};
+    }
+
+    /**
      * CP注册指令：cp.create "name" "url" ["ip"]
      * @param {*} user 
      * @param {*} paramGold 其中的成员 items 是传递给区块链全节点的参数数组
      */
     async Create(user, paramGold) {
-        console.log(paramGold.items[0]);
-        console.log(paramGold.items[1]);
-        console.log(paramGold.items[2]);
+        console.log(paramGold.items);
         let ret = await remote.execute('cp.create', paramGold.items);
         console.log(ret);
         return {code: ReturnCode.Success,result: ret};
     }
 
     /**
-     * 查询系统中现有的所有CP列表：cp.list
+     * CP修改/转让指令： cp.change "name" ["url" "ip" "addr"]
      * @param {*} user 
      * @param {*} paramGold 其中的成员 items 是传递给区块链全节点的参数数组
      */
-    async List(user, paramGold) {
-        console.log(paramGold.items[0]);
-        console.log(paramGold.items[1]);
-        console.log(paramGold.items[2]);
-        console.log(paramGold.items[3]);
-        let ret = await remote.execute('cp.list', paramGold.items);
+    async Change(user, paramGold) {
+        console.log(paramGold.items);
+        let ret = await remote.execute('cp.change', paramGold.items);
         console.log(ret);
-        return {code: ReturnCode.Success,list: ret};
+        return {code: ReturnCode.Success,result: ret};
     }
 
+    /**
+     * 根据ID查询CP注册信息 cid CP编码
+     * @param {*} user 
+     * @param {*} paramGold 其中的成员 items 是传递给区块链全节点的参数数组
+     */
+    async ById(user, paramGold) {
+        console.log(paramGold.items);
+        let ret = await remote.execute('cp.ById', paramGold.items);
+        console.log(ret);
+        return {code: ReturnCode.Success,result: ret};
+    }
+
+    /**
+     * 根据名称查询CP注册信息 name CP名称
+     * @param {*} user 
+     * @param {*} paramGold 其中的成员 items 是传递给区块链全节点的参数数组
+     */
+    async ByName(user, paramGold) {
+        console.log(paramGold.items);
+        let ret = await remote.execute('cp.ByName', paramGold.items);
+        console.log(ret);
+        return {code: ReturnCode.Success,result: ret};
+    }
 
 }
 
