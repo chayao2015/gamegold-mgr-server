b3025aaecb95922b24230fa20697e6144ebdf6b1
ThomasFuzhou <29013551@qq.com>
2018-12-07 18:09:32 +0800

    提供带权限的RemoteNode.js连接

--- app/control/CoreOfLogic/RemoteNode.js
+++ app/control/CoreOfLogic/RemoteNode.js
@@ -1,15 +1,8 @@
+//引入查看权限必备的包
+let facade = require('gamecloud');
 //引入工具包
 const toolkit = require('gamegoldtoolkit')
 
-// remote.setup({
-//     type:   'testnet',
-//     ip:     '127.0.0.1',          //远程服务器地址
-//     head:   'http',               //远程服务器通讯协议，分为 http 和 https
-//     id:     'primary',            //默认访问的钱包编号
-//     apiKey: 'bookmansoft',        //远程服务器基本校验密码
-//     cid:    'xxxxxxxx-game-gold-root-xxxxxxxxxxxx', //授权节点编号，用于访问远程钱包时的认证
-//     token:  '03aee0ed00c6ad4819641c7201f4f44289564ac4e816918828703eecf49e382d08', //授权节点令牌固定量，用于访问远程钱包时的认证
-//   });
 
 function RemoteNode() { 
     var name; 
@@ -21,7 +14,7 @@ function RemoteNode() {
     }; 
 
     // 连接并返回连接器
-    this.conn=function(userinfo) {
+    this.connY=function(userinfo) {
         console.log("remote note：");
         console.log(JSON.stringify(userinfo));
         //创建授权式连接器实例
@@ -29,6 +22,33 @@ function RemoteNode() {
         //兼容性设置，提供模拟浏览器环境中的 fetch 函数
         remote.setFetch(require('node-fetch'));
         return remote;
+    }
+
+    // 授权连接器的预备代码，调试完成后转正
+    this.conn=function(userinfo) {
+        console.log("remoteX：");
+        console.log(JSON.stringify(userinfo));
+        //创建授权式连接器实例
+        const remote = new toolkit.conn();
+        //获取cid和token
+        let operator = facade.GetObject(104, userinfo.id);
+        console.log(operator);
+        if(!!operator) {
+            //设置连接器的值
+            remote.setup({
+                type:   'testnet',
+                ip:     '127.0.0.1',          //远程服务器地址
+                head:   'http',               //远程服务器通讯协议，分为 http 和 https
+                id:     'primary',            //默认访问的钱包编号
+                apiKey: 'bookmansoft',        //远程服务器基本校验密码
+                cid:    operator.getAttr('cid'),
+                token:  operator.getAttr('token'),
+            });
+            //兼容性设置，提供模拟浏览器环境中的 fetch 函数
+            remote.setFetch(require('node-fetch'));
+            return remote;
+        }
+        return null;
     }
 }; 
 module.exports = RemoteNode;
